1ac06b9d161758b6d367451f302be5f71b9ecc6a
lekhya <lekhya.kuppam@cdk.com>
2016-07-08 09:43:16 +0530

    added first life method

--- Test.java
+++ Test.java
@@ -1,43 +1,73 @@
- import java.util.Random;
+ import java.util.Random; 
+ import java.util.Arrays;
 	class Card {
-	static int seq;
-	static String[] pip = new String[]        {"ACE","2","3","4","5","6","7","8","9","10","JACK","QUEEN","KING","JOKER","JOKER"};
+	
+	static String[] pip = new String[]        {"ACE","2","3","4","5","6","7","8","9","10","JACK","QUEEN","KING","JOKER"};
 	static String[] suite = new String[] {"club", "DIAMOND","HEART","SPADE",""};
-	public Card( int seq) {
-	this.seq = seq;
+	
+	public static int getSuite(int s) {
+	return s / 13;
 	}
-	public static String getSuite(int s) {
-	return suite[s / 13];
-	}
-	public static String getPip(int s) {
-		return pip[s % 13];	
+	public static int getPip(int s) {
+		if (s / 13 == 4) {
+		return 13;
+	} else {
+		return s % 13;	
+		}
 	}
 	public static String display(int s) {
 	int i = s / 13;
 	String suit = suite[i];
 	String p = pip [s % 13];
+	if (i == 4) {
+		p = pip[13];
+	}
 	String card = p + " " +suit;
 	return card;
 	}
-	public static void generateRandomCards() {
+	public static int[] generateRandomCards() {
 		int[] randomcards = new int[13];
 		for (int i = 0; i < 13; i++) {
-		randomcards[i] = 1+(int)(Math.random()*53);
+		randomcards[i] = (1+ (int) (Math.random() * 53));
 		System.out.println(display(randomcards[i]));
 		}
-	
+		return randomcards;
 		
 
 	}
+	/*
+	public static int[] Split(int[] randomcards) { 
+		int j = 0;
+		int[] split= new int[13];
+		for (int i = 0; i < 13 ; i++) {
+		if (randomcards[i] % 4 == j) {		
+		//split[i] = randomcards[i];
+		}
+		j++;
+		//checkLife(split);
+	}*/
+	public static void checkFirstLife() { 
+		int[] randomcards = generateRandomCards();
+		
+		Arrays.sort(randomcards);
+		System.out.println("sorted cards :");
+		
+		for (int i = 0;i < 13; i++) {
+			System.out.println(display(randomcards[i]));
+		}
+		for (int i =0; i < 11; i++)
+			if (randomcards[i] + 1 == randomcards[i + 1] && randomcards[i+1] + 1 == randomcards[i+2])
+				System.out.println("natural life"+display(randomcards[i]));		
+
+
+}
+
 	
 }
 
 public class Test {
 	public static void main (String args[]) {
-		Card c = new Card (Integer.parseInt(args[0]));
-		System.out.println(" " + c.getSuite(11));
-		System.out.println(" " + c.getPip(11));
-		System.out.println(" " + c.display(11));
-		c.generateRandomCards();
+		Card c = new Card ();
+		c.checkFirstLife();
 	}
 }
